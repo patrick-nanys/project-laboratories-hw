6cf5ad7c2c9cdff33a7eaec450919d7184b2baac
MrPatesz <patesz1999@gmail.com>
2020-03-28 20:50:08 +0100

    Inventory, Item, Rope, Shovel kommentezes.

--- skeleton/src/Shovel.java
+++ skeleton/src/Shovel.java
@@ -1,10 +1,14 @@
+/**
+ * Shovel osztaly. Az Item leszarmazottja.
+ * Segitsegevel 2 horeteg tavolithato el egy jegtablarol egy akcio alatt.
+ */
 public class Shovel extends Item {
 	/**
-	 *
-	 * @param player
+	 * Shovel hasznalatakor 2 horeteget tavolitunk el a jegtablarol, amelyiken a player all.
+	 * @param player A Shovelt hasznalo jatekos.
 	 */
 	public void use(Player player) {
-		if(!player.inSea()) {
+		if(!player.getInSea()) {
 			IceBlock ib = (IceBlock) player.getLocation();
 			FunctionLogger.log_call("IceBlock ib.modifyLayers(-2)");
 			ib.modifyLayers(-2);
@@ -12,11 +16,11 @@ public class Shovel extends Item {
 		}
 	}
 
-	/**
-	 *
-	 * @param item
-	 * @return
-	 */
+    /**
+     * Megnezi, hogy a kapott Item Shovel tipusu-e.
+     * @param item A vizsgalando Item.
+     * @return Ha Shovel a kapott Item, akkor true, egyebkent false.
+     */
 	public boolean equals(Item item) {
 		return item instanceof Shovel;
 	}
