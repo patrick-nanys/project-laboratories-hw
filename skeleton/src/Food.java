8488ac52fec6ba0ba75b5d0b5e35197b4995a3f1
szonjabalega <32750497+szonjabalega@users.noreply.github.com>
2020-03-28 17:39:41 +0200

    Food kommentek és függvények

--- skeleton/src/Food.java
+++ skeleton/src/Food.java
@@ -1,5 +1,7 @@
 /**
- *
+ *Ez az osztály reprezentál egy élelem entitást.
+ * Ha egy Player kiássa az adott IceBlockból, akkor a Playerhez kerül,
+ * aki egyből el is fogyasztja, eggyel növelve az életét.
  */
 public class Food extends Item {
 
