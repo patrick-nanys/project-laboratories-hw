3ff176f1d2379d1bebb7947ae210d5be5034b538
szonjabalega <32750497+szonjabalega@users.noreply.github.com>
2020-03-29 23:57:12 +0300

    test 11-13 changes

--- skeleton/src/Main.java
+++ skeleton/src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         Skeleton ske = new Skeleton();
         //ske.testSaveWithRope();
-        ske.testCheckCapacity();
+        ske.testUnstableFlips();
     }
 }
