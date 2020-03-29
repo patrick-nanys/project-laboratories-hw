d6d3fc324d54645fdb653a5c94bfbd4bd60250f8
patrick-nanys <patrick.nanys2000@gmail.com>
2020-03-29 18:07:08 +0200

    Player done + Item: added toString()

--- skeleton/src/Player.java
+++ skeleton/src/Player.java
@@ -26,7 +26,7 @@ public abstract class Player {
 		String name = FunctionLogger.get_obj_name();
 		ArrayList<String> p = FunctionLogger.get_parameters();
 		if (!inSea) {
-			FunctionLogger.log_call("PlayerContainerI container.movePlayer(" + name + p.get(0) + ")");
+			FunctionLogger.log_call(String.format("PlayerContainerI container.movePlayer(%s, %s)", name, p.get(0)));
 			container.movePlayer(this, d);
 			FunctionLogger.log_return("");
 		}
@@ -39,7 +39,7 @@ public abstract class Player {
 	 */
 	public boolean hasItem(Item item) {
 		ArrayList<String> p = FunctionLogger.get_parameters();
-		FunctionLogger.log_call("Inventory inventory.contains(" + p.get(0) + ")");
+		FunctionLogger.log_call(String.format("Inventory inventory.contains(%s)", p.get(0)));
 		boolean ret = inventory.contains(item);
 		FunctionLogger.log_boolean_return(ret);
 		return ret;
@@ -50,12 +50,13 @@ public abstract class Player {
 	 * és ha nincsen rajta buvarruha, akkor a jatekos meghal.
 	 */
 	public void checkPlayerStatus() {
+		String type = FunctionLogger.get_obj_type();
 		String name = FunctionLogger.get_obj_name();
 		FunctionLogger.log_call("Inventory inventory.contains(divingSuit)");
 		boolean hasDivingSuit = inventory.contains(new DivingSuit());
 		FunctionLogger.log_boolean_return(hasDivingSuit);
 		if (inSea && !hasDivingSuit) {
-			FunctionLogger.log_call("Player " + name + ".die()");
+			FunctionLogger.log_call(String.format("%s %s.die()", type, name));
 			die();
 			FunctionLogger.log_return("");
 		}
@@ -66,25 +67,27 @@ public abstract class Player {
 	 * @return tengerben van-e a jatekos
 	 */
 	public boolean getInSea() {
-		return FunctionLogger.ask_question("Vizben van a jatekos?");
+		return FunctionLogger.ask_question("Tengerben van a jatekos?");
 	}
 
 	/**
 	 * A jatekos eletero potjat eggyel csokkenti.
 	 */
 	public void loseHealth() {
+		String type = FunctionLogger.get_obj_type();
 		String name = FunctionLogger.get_obj_name();
 		if (FunctionLogger.ask_question("Kevesebb mint nulla a jatekos elete?")) {
-			FunctionLogger.log_call("Player");
+			FunctionLogger.log_call(String.format("%s %s.die()", type, name));
 			die();
+			FunctionLogger.log_return("");
 		}
-		// kerdes: kevesebb-e mint nulla az elete
 	}
 
 	/**
 	 * A jatekos megeszik egy egysegnyi etelt igy az eletero pontjat eggyel noveli.
 	 */
 	public void eat() {
+		// nen csinal semmit sem, mivel allapotot tarolna
 	}
 
 	/**
@@ -92,18 +95,54 @@ public abstract class Player {
 	 * @param item a hasznalani kivant targy
 	 */
 	public void useItem(Item item) {
+		String name = FunctionLogger.get_obj_name();
+		ArrayList<String> p = FunctionLogger.get_parameters();
+		if (!getInSea()) {
+			FunctionLogger.log_call(String.format("Inventory inventory.use(%s, %s)", p.get(0), name));
+			inventory.use(item, this);
+		}
 	}
 
 	/**
 	 * A jatekos lesopor egy egysegnyi havat a jegtablarol.
 	 */
 	public void swipeWithHand() {
+		if (!getInSea()) {
+			FunctionLogger.log_call("IceBlock ib.modifyLayers(-1)");
+			((IceBlock) container).modifyLayers(-1);
+			FunctionLogger.log_return("");
+		}
 	}
 
 	/**
 	 * A jatekos kiassa a jegtablabol a benne levo targyat.
 	 */
 	public void digOutItem() {
+		String name = FunctionLogger.get_obj_name();
+		if (!getInSea()) {
+			FunctionLogger.log_call("IceBlock ib.getLayer()");
+			IceBlock ib  = ((IceBlock) container);
+			int layer = ib.getLayer();
+			FunctionLogger.log_return("");
+			if (!FunctionLogger.ask_question("Van ho a jegtablan?")) {
+				FunctionLogger.log_call("IceBlock ib.getItem()");
+				Item item = ib.getItem();
+				String itemType = item.toString();
+				String itemName = String.valueOf(itemType.toLowerCase().charAt(0));
+				FunctionLogger.log_return(itemName);
+				FunctionLogger.log_call(String.format("%s %s.addToInventory(inventory)"));
+				boolean success = item.addToInventory(inventory);
+				FunctionLogger.log_boolean_return(success);
+				if (success) {
+					FunctionLogger.log_call("IceBlock ib.removeItem()");
+					ib.removeItem();
+					FunctionLogger.log_return("");
+					FunctionLogger.log_call(String.format("%s %s.pickUpBy(%s)", itemType, itemName, name));
+					item.pickedUpBy(this);
+					FunctionLogger.log_return("");
+				}
+			}
+		}
 	}
 
 	/**
@@ -111,11 +150,15 @@ public abstract class Player {
 	 * @return a kontener amiben a jatekos tartozkodik
 	 */
 	public PlayerContainerI getLocation() {
+		return container;
 	}
 
 	/**
 	 * Ertesiti a szintet, hogy vege a jateknak.
 	 */
 	public void die() {
+		FunctionLogger.log_call("Level levelLost()");
+		level.levelLost();
+		FunctionLogger.log_return("");
 	}
 }
