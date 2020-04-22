d019f83d6c610440f8e2b048c8cc25f3ecd29ed2
patrick-nanys <patrick.nanys2000@gmail.com>
2020-04-22 14:27:52 +0200

    Started StaticIO

--- main/src/Main.java
+++ main/src/Main.java
@@ -1,6 +1,16 @@
+import Controller.Controller;
+
 public class Main {
 
     public static void main(String[] args) {
+        if (args.length == 0) {
+            Controller c = new Controller();
+            String input = "";
+            while (!input.equals("exit")) {
 
+            }
+        } else if (args[0].equals("-test")) {
+
+        }
     }
 }
