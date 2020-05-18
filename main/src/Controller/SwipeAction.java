2a7029018689a7de64fca5eb588e3784cb99dc1f
patrick-nanys <patrick.nanys2000@gmail.com>
2020-05-18 11:12:17 +0200

    Commenting: ConsoleController, ShovelAction, SkipTurnAction, SwipeAction, ViewController

--- main/src/Controller/SwipeAction.java
+++ main/src/Controller/SwipeAction.java
@@ -1,10 +1,20 @@
 package Controller;
 
+/**
+ * Hó leseprését kezelő akció osztály.
+ */
 public class SwipeAction extends Action  {
+    /**
+     * Konstruktor ami beállítja a grafikát kezelő kontrollert.
+     * @param viewController grafikát kezelő kontroller
+     */
     public SwipeAction(ViewController viewController) {
         super(viewController);
     }
 
+    /**
+     * Meghívja a grafikát kezelő kontrollerben a hó leseprése függvényt.
+     */
     @Override
     public void click() {
         viewController.swipeSnow();
