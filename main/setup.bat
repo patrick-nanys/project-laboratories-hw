d67204955b271c92dcfb066f64fd2dc4c19ca0b8
patrick-nanys <patrick.nanys2000@gmail.com>
2020-05-18 12:32:26 +0200

    Bat files

--- main/setup.bat
+++ main/setup.bat
@@ -1,9 +1,12 @@
 @echo off
 copy src\*.java out\
-copy src\Model\*.java out\
-copy src\Controller\*.java out\
-copy src\Graphics\*.java out\
+copy src\Model\*.java out\Model\
+copy src\Controller\*.java out\Controller\
+copy src\Graphics\*.java out\Graphics\
 cd out
-javac *.java
+javac Main.java
 del *.java
+del Model\*.java
+del Controller\*.java
+del Graphics\*.java
 cd ..
